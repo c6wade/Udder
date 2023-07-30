a9a36199623c9c714108abdca13b9ce45d08b2bb
Raman Shynkarou <rshynkarou@gmail.com>
2023-07-30 08:59:36 -0400

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Raman Shynkarou on 7/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
